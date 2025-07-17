a0204f4df755ca037f77e8f10c5125afbdec470a
STela <stellamricci@icloud.com>
2025-07-17 14:20:46 +0100

    Create loops.java

---
+++ src/loops.java
@@ -0,0 +1,15 @@
+
+
+public class Loops {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++){
+            System.out.println(Math.pow(i,2));
+        }
+
+        int max = 100;
+        while (max > 0){
+            System.out.println(max);
+            max--;
+        }
+    }
+}
